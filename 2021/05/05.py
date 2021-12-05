0d864780cbaa0f294c4d561122b3bc2f9374dff5
Alexander Dahl <alex@dahl.dev>
2021-12-05 11:23:27 +0100

    Solve day 5 part 2

--- 2021/05/05.py
+++ 2021/05/05.py
@@ -2,7 +2,7 @@ from typing import Counter
 import util
 
 inp = util.input_as_lines("input")
-print(inp)
+# print(inp)
 
 seen_coords = Counter()
 
@@ -14,16 +14,40 @@ for line in inp:
         if y_start > y_end:
             y_start, y_end = y_end, y_start
         for y in range(y_start, y_end + 1):
-            print(f"{x_start},{y}")
+            # print(f"{x_start},{y}")
             seen_coords[f"{x_start},{y}"] += 1
     elif y_start == y_end:
         if x_start > x_end:
             x_start, x_end = x_end, x_start
         for x in range(x_start, x_end + 1):
-            print(f"{x},{y_start}")
+            # print(f"{x},{y_start}")
             seen_coords[f"{x},{y_start}"] += 1
+    else:
+        # 6,4 -> 2,0
+        # swap?
+        # 2, 0 -> 6,4
+        #
+        # 2, 0
+        # 3, 1
+        # 4, 2
+        # 5, 3
+        # 6, 4
+        #
+        # diagonal case
+        if x_start > x_end:
+            x_start, x_end = x_end, x_start
+            y_start, y_end = y_end, y_start
+        coords_moved = 0
+        for x in range(x_start, x_end + 1):
+            if y_start > y_end:
+                print(f"{x},{y_start-coords_moved}")
+                seen_coords[f"{x},{y_start-coords_moved}"] += 1
+            else:
+                print(f"{x},{y_start+coords_moved}")
+                seen_coords[f"{x},{y_start+coords_moved}"] += 1
+            coords_moved += 1
 
-print(seen_coords)
+# print(seen_coords)
 more_than_two = 0
 for key, val in seen_coords.items():
     if val >= 2:
