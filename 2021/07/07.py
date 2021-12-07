ac2a76d3fe71a0a9f90972208d1b16fd4891d39d
Alexander Dahl <alex@dahl.dev>
2021-12-07 10:47:24 +0100

    Solve day 7 part 2

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -13,12 +13,20 @@ last_possible = crab_list[-1]
 
 print(f"check range {first_possible}, {last_possible}")
 
+
+def calc_crab_fuel_usage(start_crab_pos, end_crab_pos):
+    # num pos to move:
+    moves = abs(start_crab_pos - end_crab_pos)
+    # each move is a 1 + 2 + 3 + 4 sum
+    return int((moves * (moves + 1)) / 2)
+
+
 best_crab_pos = None
-best_crab_fuel = 9999999
+best_crab_fuel = 999999999999
 for crab_pos in range(first_possible, last_possible):
     cur_crab_fuel = 0
     for crab in crab_list:
-        cur_crab_fuel += abs(crab - crab_pos)
+        cur_crab_fuel += calc_crab_fuel_usage(crab, crab_pos)
     print(f"pos: {crab_pos} used {cur_crab_fuel} fuel")
     if cur_crab_fuel < best_crab_fuel:
         best_crab_fuel = cur_crab_fuel
