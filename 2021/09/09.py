92b80fe156187a6ced08ab2038770d303017ae3d
Alexander Dahl <alex@dahl.dev>
2021-12-09 09:13:17 +0100

    Solve day 9 part 2

--- 2021/09/09.py
+++ 2021/09/09.py
@@ -4,6 +4,7 @@ inp = util.input_as_lines("input")
 
 risk_level = 0
 
+low_points = []
 for y in range(len(inp)):
     for x in range(len(inp[y])):
         cur_height = inp[y][x]
@@ -47,5 +48,70 @@ for y in range(len(inp)):
             and lower_than_east
             and lower_than_west
         ):
+            low_points.append((y, x))
             risk_level += int(cur_height) + 1
 print(risk_level)
+
+G = {}
+# Let's do a graph instead
+for y in range(len(inp)):
+    for x in range(len(inp[y])):
+        G[(y, x)] = []
+        try:
+            if y - 1 >= 0:
+                G[(y, x)].append([(y - 1, x), int(inp[y - 1][x])])
+        except:
+            print("list out of range")
+        try:
+            if y + 1 < len(inp):
+                G[(y, x)].append([(y + 1, x), int(inp[y + 1][x])])
+        except:
+            print("list out of range")
+        try:
+            if x + 1 < len(inp[y]):
+                G[(y, x)].append([(y, x + 1), int(inp[y][x + 1])])
+        except:
+            print("list out of range")
+        try:
+            if x - 1 >= 0:
+                G[(y, x)].append([(y, x - 1), int(inp[y][x - 1])])
+        except:
+            print("list out of range")
+print(G)
+print(G.keys())
+print(G[(4, 9)])
+
+
+def find_basins(graph, start_node):
+    visited_nodes = []
+    next_nodes = [start_node]
+    while next_nodes:
+        next_node = next_nodes.pop()
+        for node in graph[tuple(next_node)]:
+            if node not in visited_nodes:
+                if node[1] == 9:
+                    print(f"{node[0]} is a peak!")
+                else:
+                    print(f"{node[0]} is not a peak!")
+                    visited_nodes.append(node)
+
+                    next_nodes.append(node[0])
+
+    print(visited_nodes)
+    print(len(visited_nodes))
+    return len(visited_nodes)
+
+
+basin_size = []
+
+for points in low_points:
+    #  From the graph, find all basins!
+    print(points)
+    print(G[points], points)
+    basin_size.append(find_basins(G, points))
+
+basin_size.sort()
+import math
+
+print(basin_size[-3::])
+print(math.prod(basin_size[-3::]))
