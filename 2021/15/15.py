33fe46834ccdcac3ffe7dabae0e15df3fa8d758a
Alexander Dahl <alex@dahl.dev>
2021-12-16 08:40:40 +0100

    Solve day 15 part 2

--- 2021/15/15.py
+++ 2021/15/15.py
@@ -9,24 +9,38 @@ G = nx.DiGraph()
 
 cur_inp = inp
 # make a graph, then dijikstra?
+# make the new input, its eaiser than adjusting the graph structure
+
 weights = {}
-for y in range(len(cur_inp)):
-    for x in range(len(cur_inp)):
-        weights[(y, x)] = int(cur_inp[y][x])
+new_inp = []
+size = len(cur_inp)
+for y in range(size * 5):
+    new_row = []
+    for x in range(size * 5):
+        new_val = int(cur_inp[y % size][x % size]) + (x // size) + (y // size)
+        new_val = new_val % 9
+        if new_val == 0:
+            new_val = 9
+        new_row.append(new_val)
+    new_inp.append(new_row)
+
+for y in range(len(new_inp)):
+    for x in range(len(new_inp)):
+        weights[(y, x)] = int(new_inp[y][x])
         if y - 1 >= 0:
-            G.add_edge((y, x), (y - 1, x), weight=int(cur_inp[y - 1][x]))
-        if y + 1 < len(cur_inp):
-            G.add_edge((y, x), (y + 1, x), weight=int(cur_inp[y + 1][x]))
-        if x + 1 < len(cur_inp[y]):
-            G.add_edge((y, x), (y, x + 1), weight=int(cur_inp[y][x + 1]))
+            G.add_edge((y, x), (y - 1, x), weight=int(new_inp[y - 1][x]))
+        if y + 1 < len(new_inp):
+            G.add_edge((y, x), (y + 1, x), weight=int(new_inp[y + 1][x]))
+        if x + 1 < len(new_inp[y]):
+            G.add_edge((y, x), (y, x + 1), weight=int(new_inp[y][x + 1]))
         if x - 1 >= 0:
-            G.add_edge((y, x), (y, x - 1), weight=int(cur_inp[y][x - 1]))
+            G.add_edge((y, x), (y, x - 1), weight=int(new_inp[y][x - 1]))
 
-import pprint
-
-pprint.pprint(nx.to_dict_of_dicts(G))
 shortest_path = nx.shortest_path(
-    G, source=(0, 0), target=(len(cur_inp) - 1, len(cur_inp) - 1), weight="weight"
+    G,
+    source=(0, 0),
+    target=((len(cur_inp) * 5) - 1, (len(cur_inp) * 5) - 1),
+    weight="weight",
 )
 print(shortest_path)
 
