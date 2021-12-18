e58dcf8fd3b9d3da5cf3c65b53db8ec1553fde61
Alexander Dahl <alex@dahl.dev>
2021-12-18 15:49:47 +0100

    Solve day 14 part 2

--- 2021/14/14.py
+++ 2021/14/14.py
@@ -4,35 +4,43 @@ inp = util.input_as_lines("input")
 sample = util.input_as_lines("sample_input")
 
 start_polymer = inp[0]
+last_char = start_polymer[-1]
 print(start_polymer)
 
 polymer_rules = {}
 
 for line in inp[2:]:
     rule, insertion = line.split(" -> ")
-    polymer_rules[rule] = insertion
+    polymer_rules[rule] = [rule[0] + insertion, insertion + rule[1]]
+
+print(polymer_rules)
+from collections import Counter
 
 polymer = start_polymer
 
-for step in range(1, 11):
-    new_polymer = ""
-    for i in range(0, len(polymer)):
-        if i == 0:
-            new_polymer += polymer[i]
-            continue
-        pair = f"{polymer[i-1]}{polymer[i]}"
-        if pair in polymer_rules:
-            new_polymer += f"{polymer_rules[pair]}{polymer[i]}"
+pairs = Counter()
+for i in range(0, len(polymer)):
+    if i == 0:
+        continue
+    pair = f"{polymer[i-1]}{polymer[i]}"
+    pairs[pair] += 1
+
+print(pairs)
+for step in range(1, 41):
+    new_pairs = Counter()
+    char_counter = Counter()
+    for key in pairs.keys():
+        rule_output = polymer_rules[key]
+        num_of_rules = pairs[key]
+        for rule in rule_output:
+            print(f"key {key} gave rule {rule}")
+            new_pairs[rule] += num_of_rules
+            char_counter[rule[0]] += num_of_rules
+    pairs = new_pairs.copy()
     print(f"step: {step}")
-    print(new_polymer)
-    polymer = new_polymer
-print(polymer)
+    print(pairs)
+    print(char_counter)
 
-from collections import Counter
-
-char_count = Counter()
-for char in set(polymer):
-    char_count[char] = polymer.count(char)
-
-print(char_count)
-print(char_count.most_common()[0][1] - char_count.most_common()[-1][1])
+# +1 since there is always the last_char in the series we dont count
+char_counter[last_char] += 1
+print(char_counter.most_common()[0][1] - char_counter.most_common()[-1][1])
